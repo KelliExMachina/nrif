1f3aa4eff918f482b38649bb92785b0141f9e115
Kelli Kennedy <kelliexmachina@gmail.com>
2020-04-30 14:34:53 -0500

    Update file sort based on class.  add file_proc

--- src/file_proc.py
+++ src/file_proc.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 
 def build_image_list(path):
@@ -14,3 +15,20 @@ def build_image_list(path):
         for f in f_names:
             fname.append(os.path.join(root, f))
     return fname
+
+
+def sort_images(path):
+    '''
+    Search (path) and copy each file to a folder labelled by the last digit of the filename.
+    INPUTS: path = top of directory tree to begin recursion for images. ex) "../data"
+    OUTPUTS: sorted images
+    '''
+    for root, d_names, f_names in os.walk(path):
+        f_names = [f for f in f_names if not f[0] == '.']  # skip hidden files: .DSstore
+        d_names = [d for d in d_names if not d[0] == '.']  # skip hidden folders: .git
+        for f in f_names:
+            face_class = f[-5]
+            dest_base = os.path.join('../data/train', face_class, f)
+            orig_base = os.path.join(path, f)
+            shutil.copy(orig_base, dest_base)
+    return 1
